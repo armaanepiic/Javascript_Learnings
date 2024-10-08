635f96c5ce485bcaec861019bc808af9c01dd0c8
Arman Hossain <armaanepiicfb17@gmail.com>
2024-10-08 12:16:35 +0600

    variable declaration

---
+++ Js/script.js
@@ -0,0 +1,25 @@
+var a, b, c;
+a = 10, b = 20, c = a + b;
+console.log(c);
+var $d, _c, ab21, myname, MyName;
+
+var a = 50;
+console.log(a);
+
+
+
+var a;
+let b;
+a = 45;
+console.log(a);
+a = 35.45;
+console.log(a);
+a = "Arman";
+console.log(a);
+b = a;
+console.log(b);
+a = 69;
+console.log(a);
+console.log(b)
+
+
