1628212196bc6c8bcf576b65ad7edb5cc2de3bc5
Arman Hossain <armaanepiicfb17@gmail.com>
2024-10-08 19:58:39 +0600

    numbers and string data type

---
+++ Js/data-types.js
@@ -0,0 +1,65 @@
+
+// string
+var x;
+
+// var x = "Hello "f" world"; // error
+x = "Hello \"trt\" world"
+x = 'Hello \'trt\' world';
+x = "Hello 'good' world";
+x = 'Hello "good" \\trt\\ world';
+x = "hello \t world"; // \t -> tab
+x = "hello \n world";
+x = "Hello";
+var y = " World";
+var z = x + y;
+
+
+console.log(z);
+console.log(z.length);
+
+// console.log(x);
+
+
+
+
+/*
+// Numbers
+var x;
+x = 5; // 5
+x = 3.1416; // 3.1416
+x = 123e5; // 12300000
+x = 123e-8; // 0.00000123
+x = "34" + "45"; // 3445
+x = "10" + 20; // 1020
+x = 10 + "20"; // 1020
+x = 10 + 5 + "45"; // 1545
+x = "25" - 5; // 20
+x = "35" - "5"; // 30
+x = 5 / 0; // Infinity
+x = Infinity; // Infinity 
+x = 0xA; // 10
+x = 187;
+x = 3.1416;
+x = 10;
+var y = x.toString(2);
+var y = x.toString(8);
+var y = x.toString(16);
+x = 3.1416;
+y = x.toPrecision(8); // 3.1416000
+x = 187;
+y = x.toString();
+var z = parseInt(y);
+y = "3.1416";
+var z = parseFloat(y);
+x = 3.1416;
+z = parseFloat(x.toString());
+
+var x = "sfgs"; // t
+x = "123"; // f
+x = "123f"; // t
+
+z = isNaN(x);
+
+// console.log(x);
+console.log(z); 
+*/
