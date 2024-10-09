617c98bbc5890957337a54dec5bb1488285d8ab3
Arman Hossain <armaanepiicfb17@gmail.com>
2024-10-09 13:57:47 +0600

    object finished

---
+++ Js/object.js
@@ -0,0 +1,39 @@
+// objects
+
+var arr = [10, 20, ["a", "b", ["UK", "USA"], "c"], 30, 40];
+
+arr = [1, 2, ["a", "b"], { prop1: "item 1", prop2: "item 2" }];
+
+console.log(arr[3].prop2); // item 2
+console.log(arr);
+// console.log(arr[2][2][0]); // UK
+
+
+
+
+
+/*
+
+var student = {
+    Name: "Arman Hossain",
+    Age: 27,
+    Contact: "01874015451",
+    City: "Chittagong",
+    Country: "Bangladesh"
+}
+console.log(student["Name"]);
+console.log(student.Age);
+student["Occupation"] = "Tution";
+console.log(student);
+student["weight"] = "70kg";
+console.log(student);
+
+delete student.Age; 
+
+student = {};
+console.log(student);
+student.name = "Nusrat";
+student.age = "25";
+student.City = "Ctg";
+console.log(student);
+*/
