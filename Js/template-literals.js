b840a8e780a193552dd5cbd88fcd1014f65c5915
Arman Hossain <armaanepiicfb17@gmail.com>
2024-10-12 20:40:29 +0600

    template literals completed

---
+++ Js/template-literals.js
@@ -0,0 +1,21 @@
+// Template literals (ES6)
+// console.log("first line\nsecond line");
+// Backtick
+let a = `First Line
+Second Line`;
+console.log(a);
+
+let age = 27;
+console.log(`His age is ${age}`);
+let name = "Arman", dob = "30 April 1997";
+console.log(`His name is ${name}
+His age is ${age}
+His date of birth is ${dob}`);
+
+let x = 34, y = 45;
+console.log(`${x} + ${y} = ${x + y}`);
+
+
+
+
+
