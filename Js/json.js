e6505bd89abb99035ce52442331fce0985ab752a
Arman Hossain <armaanepiicfb17@gmail.com>
2024-10-26 21:57:20 +0600

    JSON to Object & Object to JSON

---
+++ Js/json.js
@@ -0,0 +1,13 @@
+var student = {
+    name: "arman",
+    age: 26,
+    hometown: "Dhaka"
+};
+var student_json = JSON.stringify(student);
+
+console.log(student_json);
+
+var student_new = JSON.parse(student_json);
+
+console.log(student_new);
+
