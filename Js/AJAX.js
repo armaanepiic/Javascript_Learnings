5cbb2da8b711161e05705aa285874bcd82aa644f
Arman Hossain <armaanepiicfb17@gmail.com>
2024-10-27 09:42:30 +0600

    learned ajax basics

--- Js/AJAX.js
+++ Js/AJAX.js
@@ -1,10 +1,34 @@
 // Asynchronus Programming
 // AJAX = Asynchronus Javascript and XML
 
+// status code
+// HTTP statuses
+// 200: "OK"
+// 403: "Forbidden"
+// 404: "Not Found"
+
 document.getElementById("get_data").addEventListener('click', loadData);
 
 function loadData() {
+
     // console.log("Button clicked");
+    // create XHR object (XML HTTP REQUEST)
+
+    let xhr = new XMLHttpRequest();
+    
+    // open function
+    xhr.open('GET', 'Data.txt', true);
+    xhr.onload = function () {
+        if (this.status === 200) {
+            // console.log(this.responseText);
+            // var temp = this.responseText;
+            document.getElementById("output").innerHTML =`<h4>${this.responseText}</h4>`;
+            
+        }
+    }
+    xhr.send();
+    
+    // console.log(xhr);
 }
 
 
