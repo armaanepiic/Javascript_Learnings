f79bda1c6c2f852e5108a4d5a91731d500bf9d84
Arman Hossain <armaanepiicfb17@gmail.com>
2024-10-27 09:26:10 +0600

    Ajax Started

---
+++ Js/AJAX.js
@@ -0,0 +1,15 @@
+// Asynchronus Programming
+// AJAX = Asynchronus Javascript and XML
+
+document.getElementById("get_data").addEventListener('click', loadData);
+
+function loadData() {
+    // console.log("Button clicked");
+}
+
+
+
+
+
+
+
--- Js/json.js
+++ Js/json.js
@@ -1,3 +1,50 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+// JSON doesn't support undefined, date, function
+
+// JSON.stringigy() -> JS Object to JSON string
+// JSON.parse() -> JSON String to JS Object
+
+var person1 = {
+    name: "arman", // string
+    age: 25, // number
+    hometown: "Dhaka",
+    married: false, // boolean
+}
+var person2 = {
+  "name": "arman", // string
+  "age": 25, // number
+  "hometown": "Dhaka",
+  "married": false, // boolean
+};
+
+var person1_json = JSON.stringify(person1);
+var person1_obj = JSON.parse(person1_json);
+console.log(person1_obj);
+console.log(person1.name);
+
+
+// console.log(person1);
+// console.log(person2);
+
+
+// var person_json = JSON.stringify(person);
+// console.log(per);
+
+
 var student = {
     name: "arman",
     age: 26,
@@ -10,4 +57,4 @@ console.log(student_json);
 var student_new = JSON.parse(student_json);
 
 console.log(student_new);
-
+*/
