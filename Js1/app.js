57034ea07ed06c4689fa72c43c8299645985f925
Arman Hossain <armaanepiicfb17@gmail.com>
2024-10-28 11:32:06 +0600

    truthy value and falsy value

--- Js1/app.js
+++ Js1/app.js
@@ -15,9 +15,29 @@ console.log(typeof str);
 
 let nothing = null;
 console.log(typeof nothing);
-*/
+
 let num = 123;
 console.log(typeof num); // number
 let stringFromNum = String(num); // String() converts a number to string
 console.log(typeof stringFromNum); // sring
+
+let num = 10;
+let num2 = "4";
+console.log(typeof num); // number
+console.log(typeof num2); // string
+console.log(num * num2); // 40 (numbe)
+let num3 = "3a";
+console.log(num * num3); // NaN
+*/
+
+// truthy value
+// falsy value -> 0, -0, '', undefined, null, NaN
+
+console.log(Boolean(""));
+
+
+
+
+
+
 
