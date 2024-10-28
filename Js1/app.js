56b9553055856da372e1ca9d38b6ddd75ea4e74f
Arman Hossain <armaanepiicfb17@gmail.com>
2024-10-28 11:10:00 +0600

    data type, typecasiting started

---
+++ Js1/app.js
@@ -0,0 +1,23 @@
+/*
+let str = "arman";
+str = "12345"
+console.log(typeof str); // string
+
+let num = 12.36;
+console.log(typeof num);
+
+let isMarried = true;
+console.log(typeof isMarried);
+
+let str;
+console.log(typeof str);
+
+
+let nothing = null;
+console.log(typeof nothing);
+*/
+let num = 123;
+console.log(typeof num); // number
+let stringFromNum = String(num); // String() converts a number to string
+console.log(typeof stringFromNum); // sring
+
