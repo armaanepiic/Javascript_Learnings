5c4ba16f1983b8aa52151fb2528ea34b1e5cfbf3
Arman Hossain <armaanepiicfb17@gmail.com>
2024-11-17 13:15:18 +0600

    add numbers

--- Js1/app.js
+++ Js1/app.js
@@ -1,48 +1,10 @@
-/*
-let str = "arman";
-str = "12345"
-console.log(typeof str); // string
 
-let num = 12.36;
-console.log(typeof num);
-
-let isMarried = true;
-console.log(typeof isMarried);
-
-let str;
-console.log(typeof str);
-
-
-let nothing = null;
-console.log(typeof nothing);
-
-let num = 123;
-console.log(typeof num); // number
-let stringFromNum = String(num); // String() converts a number to string
-console.log(typeof stringFromNum); // sring
-
-let num = 10;
-let num2 = "4";
-console.log(typeof num); // number
-console.log(typeof num2); // string
-console.log(num * num2); // 40 (numbe)
-let num3 = "3a";
-console.log(num * num3); // NaN
-
-
-// truthy value
-// falsy value -> 0, -0, 0n, '', undefined, null, NaN
-
-console.log(Boolean(""));
-console.log('23' % 5); // reminder 3
-console.log(2 ** 3);
-*/
-let num = 15;
-// num = num + 1;
-// let num2 = num++;
-let num2 = ++num;
-
-console.log(num2);
-console.log(num);
-
+function addNumbers(num1 = 0, num2 = 0) {
+    console.log(num1 + num2);
+    console.log(arguments);
+    
+}
+addNumbers(1,2,3,4,5,6,7)
+    
+    
 
