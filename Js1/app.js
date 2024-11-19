55ee8a98bb25ca5a0ca467f65c69c184d41d5537
Arman Hossain <armaanepiicfb17@gmail.com>
2024-11-19 10:08:03 +0600

    forEach Function

--- Js1/app.js
+++ Js1/app.js
@@ -1,11 +1,54 @@
+// For each
+var foods = ["Cake", "Ice Cream", "Donut", "Mango"];
+var numbers = [1, 3, 5, 6, 7, 9];
+// 1st parameter: item
+// 2nd parameter: index
+// 3rd parameter: whole array
+
+foods.forEach(function (item, index, arr) {
+    console.log(item, index);
+    console.log(arr);
+});
+
+
+
+
+
+
+
+/*
+// Normal Declaration
+function sayHello(x) {
+    console.log('Hello ' + x);
+}
+// Function Expression
+let sayHello1 = function (x) {
+    console.log("hello " + x + " again");
+}
+sayHello("Arman");
+sayHello1("Arman");
+
+// Arrow Function
+let sayHello2 = (el) => {
+    console.log("I am an arrow function " + el);
+}
+sayHello2("Hi");
+*/
+
+
+
+
+
+
 // Function
 // User Defined Function
-
+/*
 function sayHello() {
     console.log("hello");
     console.log("I am learning JS");
 }
 let x = sayHello();
+*/
 
 
 
