90d7056e4d0834e1ce4b74e046040c596cda8a72
Arman Hossain <armaanepiicfb17@gmail.com>
2024-11-19 09:39:44 +0600

    i just rewatch function today

--- Js1/app.js
+++ Js1/app.js
@@ -1,36 +1,25 @@
-// Constructor Function
-function CreatePerson(name, age) {
-    this.name = name;
-    this.age = age;
-    this.sayHello = function(){
-        console.log("Hello");
-    };
+// Function
+// User Defined Function
+
+function sayHello() {
+    console.log("hello");
+    console.log("I am learning JS");
 }
-let person1 = new CreatePerson("arman", 26);
-let person2 = new CreatePerson("nusrat", 24);
-console.log(person1);
-console.log(person2);
+let x = sayHello();
 
 
-// Factory Function
-// function createPerson(name, age, location) {
-//     return {
-//         name,
-//         age,
-//         location,
-//         sayHello() {
-//             console.log("Hello " + name);
-//         }
-//     };
-// }
-// let person1 = createPerson("Arman", 26, "Chattogram");
-// let person2 = createPerson("Nusrat", 24, "Sitakund");
-// let person3 = createPerson("Jane", 20, "USA");
 
-// console.log(person1);
-// console.log(person2);
-// console.log(person3);
-// person1.color = "Red";
-// console.log(person1);
-// console.log(person2);
-// console.log(person3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
