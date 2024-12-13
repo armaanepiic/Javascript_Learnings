e81893f6c3dd9723b259c7cc0e491f079bd233dd
Arman Hossain <armaanepiicfb17@gmail.com>
2024-12-13 14:27:59 +0600

    querySelector()

--- JS_practice_phase_2/js_refresher.js
+++ JS_practice_phase_2/js_refresher.js
@@ -35,22 +35,47 @@ linkArr.forEach((element) => {
   console.log(element);
 });
 console.log(links);
-*/
+
 
 // DOM Selector
 // document.getElementById()
 let val, value;
-
 val = document.getElementById('document_title');
+
 // accessing the property
+
 value = val.className;
 value = val.style.background = '#333';
 value = val.style.color = '#fff';
 value = val.style.padding = '10px';
 value = val.style.display = 'block';
+
 // changing content
+
 value = val.textContent = 'new title';
 value = val.innerText = 'again new title';
 value = val.innerHTML = "<i>Italic heading</i>";
-
 console.log(value);
+
+
+
+// querySelector()
+// can be called by class(.className), id(#id), tagName
+let val, value;
+val = document.querySelector('#document_title'); // id
+val = document.querySelector('.title-class') // class
+val = document.querySelector('h3');
+val = document.querySelector('ol'); // show the first ol
+val = document.querySelector('li'); // show the first li tag element
+val = document.querySelector('ol li') // show the first li tag element inside the first ol tag
+val.style.background = 'red';
+val.style.color = 'white';
+val = document.querySelector('li:last-child')
+val = document.querySelector('li:nth-child(2)')
+
+console.log(val);
+*/
+
+// Multiselector
+// document.getElementByClassName()
+
