57b5820a3c4cf4b882adaa1ce802d231ef6c553a
Arman Hossain <armaanepiicfb17@gmail.com>
2024-12-20 11:59:11 +0600

    Template literals

---
+++ JS_ES6/Template_literals.js
@@ -0,0 +1,9 @@
+// template literals
+let age = 26;
+let name = "Arman Hossain";
+console.log(`My name is ${name}
+I am form Bangladesh
+My age is ${age}`);
+
+let a = 10, b = 20;
+console.log(`Sum  = ${a+b}`)
