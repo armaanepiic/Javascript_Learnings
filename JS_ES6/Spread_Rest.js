b8829193b3b3861b060d31468ff0c1de2f4c33c1
Arman Hossain <armaanepiicfb17@gmail.com>
2024-12-20 17:02:13 +0600

    Rest Operator

--- JS_ES6/Spread_Rest.js
+++ JS_ES6/Spread_Rest.js
@@ -20,7 +20,7 @@ let person = {
 }
 let newPerson = { ...person , dob: "30-04-1997"};
 console.log(newPerson);
-*/
+
 
 let numbers = [12, 1, 0, -2];
 console.log(Math.max(numbers)); // NaN
@@ -31,3 +31,32 @@ let test = (fname, lname) => {
     console.log(`Hello ${fname} ${lname}`);
 }
 test(...name);
+*/
+
+// Rest operator
+// rest on array
+let fruits = ["Apple", "Banana", "Orange", "Guava"];
+let [first, second, ...rest] = fruits;
+console.log(first);
+console.log(second);
+console.log(rest);
+
+// rest on object
+let person = {
+    fname: "Arman",
+    lname: "Hossain",
+    dob: "30-04-1997",
+    country: "Bangladesh",
+    contact: "01874015451"
+}
+let { fname , lname,  ...others} = person;
+console.log(fname);
+console.log(others);
+
+// passing as argument
+let moreNum = [78, 56, -62, -46];
+let test = (name, ...numbers) => {
+    console.log(name);
+    console.log(numbers);
+}
+test("arman", ...moreNum, 12, 96, 0, -14);
