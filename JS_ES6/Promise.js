5f8b2d8750dff37e9a96ec4b8e9aa63820fd98cc
Arman Hossain <armaanepiicfb17@gmail.com>
2024-12-20 19:51:17 +0600

    promise async await

--- JS_ES6/Promise.js
+++ JS_ES6/Promise.js
@@ -1,7 +1,7 @@
 // Promise
 // successful -> resolve
 // failed -> reject
-
+/*
 let prom = new Promise((resolve, reject) => {
     let a;
     setTimeout(() => {
@@ -25,3 +25,25 @@ console.log("I am after promise");
 
 
 // used in image file
+
+*/
+
+// console.log(fetch("https://api.chucknorris.io/jokes/random"));
+// fetch("https://api.chucknorris.io/jokes/random").then(response => response.json()).then(data => console.log(data));
+// console.log("hello world");
+
+// async and await
+
+fetch("https://api.chucknorris.io/jokes/random")
+  .then((response) => response.json())
+  .then((data) => {});
+
+async function getJokes() {
+    let response = await fetch("https://api.chucknorris.io/jokes/random");
+    let data = await response.json();
+    return data;
+}
+
+getJokes().then(jokes => {
+    console.log(jokes.value);    
+})
