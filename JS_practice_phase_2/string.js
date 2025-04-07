a9f85a0496e74f7a6384453e9883387dd1137e9d
Arman Hossain <armaanepiicfb17@gmail.com>
2025-04-07 13:20:34 +0600

    string methods

---
+++ JS_practice_phase_2/string.js
@@ -0,0 +1,96 @@
+var x = "Hello World";
+x = "Hello ''d''' World";
+x = 'Hello World';
+x = 'Hello ""d"" World'
+x = "Hello \"\" World";
+x = 'Hello \'\' World'
+x = "Backslash = \\";
+x = "Hello \t World";
+x = "Hello \nWorld";
+x = "Hello";
+var y = " World";
+var z = x + y;
+x = 'Hello World'; // length 11
+x = 'Hello \nWorld'; // length 12
+z = x[0]; // z = H
+z = x[19]; // z = undefined
+
+// string methods
+
+// str.toUpperCase()
+x = "Bohubrihi";
+z = x.toUpperCase();
+x = "BOHUBRIHI";
+
+// str.toLowerCase()
+z = x.toLowerCase();
+
+// str.trim()
+x = "  Bohubrihi  ";
+z = x.trim();
+z = x.trimStart();
+
+
+// str.slice()
+x = "Bohubrihi";
+z = x.slice(1, 4); // ohu
+z = x.slice(-5, -1); // brih
+z = x.slice(2); // hubrihi
+
+
+// str.substr(start_index, length )
+x = "Bohubrihi";
+z = x.substr(3, 2); // ub
+z = x.substr(3); // ubrihi
+
+// str.replace("characters, new_characters")
+x = "Bohubrihi";
+z = x.replace("hu", "ba");
+
+// str.concat
+x = "ABC";
+z = x.concat("EFG", "123", 567);
+
+// str.charCodeAt()
+x = "Hello";
+z = x.charCodeAt(0); // 72 (Ascii Value of H = 72)
+
+// str.trimstart()
+x = "    Bohubrihi    ";
+z = x.trimStart();  // Bohubrihi----
+z = x.trimEnd();
+
+// str.padStart(length, character)
+x = "a";
+z = x.padStart(4, '0');
+
+// str.paddEnd()
+x = 'a';
+z = x.padEnd(4, '0');
+
+// str.repeat(number of repeatation)
+x = "hello ";
+z = x.repeat(3);
+
+// str.replaceAll()
+x = "Cat is nice. I have a Cat";
+z = x.replaceAll('Cat', "Dog");
+
+// string to array
+// str.split()
+x = "Hello";
+z = x.split("");
+for (let index = 0; index < z.length; index++) {
+    console.log(z[index]);    
+}
+x = "I love Bangladesh";
+z = x.split(' ');
+for (let index = 0; index < z.length; index++) {
+  console.log(z[index]);
+}
+
+console.log(`z = ${z}`); 
+// console.log(`x = ${x}`);
+// console.log(`Length of ${x} = ${x.length}`);
+
+
