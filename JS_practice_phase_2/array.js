7f6af9952fe0bdbc22fb6e3793ce546f4059f31d
Arman Hossain <armaanepiicfb17@gmail.com>
2025-04-09 22:56:57 +0600

    array basics

---
+++ JS_practice_phase_2/array.js
@@ -0,0 +1,43 @@
+var countries = ["Bangladesh", "UK", "Palestine", "Peru"];
+countries[1] = "Poland"; // array is mutable
+
+
+// adding element at the end of the array
+countries[countries.length] = "Norway";
+// push
+countries.push("Ireland");
+// pop
+countries.pop();
+
+// shift to delete from the beginning
+countries.shift();
+
+// unshift to add at the beginning
+countries.unshift("Bangladesh");
+
+var numbers = [];
+
+// console.log(numbers);
+
+
+// var length = countries.length;
+// console.log(length);
+
+// console.log(countries);
+// countries.forEach(element => {
+//     console.log(element);
+// });
+
+
+// string to array
+var x = "I live in Bangladesh";
+var y = x.split(' ');
+x = "Bangladesh, China, Finland";
+y = x.split(', ');
+var z = y;
+x = z.toString(); // insert comma
+x = z.join(' '); // make a string inserting white space
+
+console.log(z);
+
+console.log(x);
